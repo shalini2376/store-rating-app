c59de267347824d54eba143cce190f36771676ca
shalini2376 <shalinisingh2376@gmail.com>
2026-01-15 02:35:19 +0530

    add live backend baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://store-rating-app-avmf.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
